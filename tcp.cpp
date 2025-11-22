9dc88697a45dea8a638d3866535173103392489c
kklaha <55ovsep55@gmail.com>
2025-11-22 13:38:07 +0300

    Update tcp.cpp

--- tcp.cpp
+++ tcp.cpp
@@ -1,58 +1,64 @@
-﻿#include <iostream>
-#include <winsock2.h>
-#include <ws2tcpip.h>
-#pragma comment(lib, "Ws2_32.lib")
+#include <iostream>  
+#define _WINSOCK_DEPRECATED_NO_WARNINGS  
+#include <winsock2.h> 
 #include <string>
+#include <windows.h>
+#pragma comment (lib, "Ws2_32.lib")
+#pragma warning(disable: 4996)  // Отключает предупреждения о deprecated функциях
+
 using namespace std;
-#define PORT 1234
-#define BUF_SIZE 64
+
+#define SRV_HOST "localhost"  
+#define SRV_PORT 1234 
+#define CLNT_PORT 1235  
+#define BUF_SIZE 64  
+
+char TXT_ANSW[] = "I am your student\n";  
 
 int main() {
-    // Инициализируем Winsock
-    WSADATA wsa;
-    if (WSAStartup(MAKEWORD(2, 2), &wsa) != 0) {
-        cout << "WSA failed\n";
-        return 1;
+    char buff[1024]; 
+
+    if (WSAStartup(0x0202, (WSADATA*)&buff[0])) {
+        cout << "Error WSAStartup \n" << WSAGetLastError();
+        return -1;
     }
 
-    // Создаём сокет
-    SOCKET sock = socket(AF_INET, SOCK_STREAM, 0);
-    if (sock == INVALID_SOCKET) {
-        cout << "Socket failed\n";
-        WSACleanup();
-        return 1;
-    }
+    SOCKET s; 
+    int from_len;   
+    char buf[BUF_SIZE] = {0};    
+    hostent * hp;
+    sockaddr_in clnt_sin, srv_sin;  
 
-    // Говорим, КУДА подключаться (сервер: localhost:1234)
-    sockaddr_in server;
-    server.sin_family = AF_INET;
-    server.sin_port = htons(PORT);
+    s = socket(AF_INET, SOCK_STREAM, 0); 
 
-    inet_pton(AF_INET, "127.0.0.1", &server.sin_addr); // IP: localhost
+    clnt_sin.sin_family = AF_INET; 
+    clnt_sin.sin_addr.s_addr = 0; 
+    clnt_sin.sin_port = htons(CLNT_PORT); 
 
-    // Подключаемся к серверу
-    if (connect(sock, (sockaddr*)&server, sizeof(server)) == SOCKET_ERROR) {
-        cout << "Connect failed\n";
-        closesocket(sock);
-        WSACleanup();
-        return 1;
-    }
-    cout << "Connected to server!\n";
-    char buf[BUF_SIZE];
-    string msg;
-    // Основной цикл: получаем вопрос → вводим ответ
-    while (true) {
-        int bytes = recv(sock, buf, BUF_SIZE - 1, 0);
-        if (bytes <= 0) break;  // Сервер отключился
-        buf[bytes] = '\0';
-        cout << buf;  // Печатаем вопрос
-        getline(cin, msg);
-        send(sock, msg.c_str(), msg.size(), 0);
+    bind(s, (sockaddr*)&clnt_sin, sizeof(clnt_sin)); 
 
-        if (msg == "Bye") break;
-    }
-    // Завершаем работу
-    closesocket(sock);
-    WSACleanup();
-    return 0;
+    hp = gethostbyname(SRV_HOST);
+    srv_sin.sin_port = htons(SRV_PORT);
+    srv_sin.sin_family = AF_INET; 
+
+    ((unsigned long*)&srv_sin.sin_addr)[0] =
+        ((unsigned long**)hp->h_addr_list)[0][0];
+
+    connect(s, (sockaddr*)&srv_sin, sizeof(srv_sin));
+
+    string mst;
+    do {
+        from_len = recv(s, (char*)&buf, BUF_SIZE, 0); 
+        buf[from_len] = 0;
+        cout << buf << endl;
+
+        getline(cin, mst);
+        int msg_size = mst.size();
+        send(s, (char*)&mst[0], msg_size, 0); 
+    } while (mst != "Bye");
+
+    cout << "exit to infinity" << endl;
+    cin.get();   
+    closesocket(s); 
+    return 0;  
 }
